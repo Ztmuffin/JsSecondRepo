265236e17ed000160a57ae425ad35399254a526d
Addison Moyer <addisonbmoyer@gmail.com>
2016-09-28 11:50:14 -0600

    it works

---
+++ js/event.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('button1');
+
+var moosFunction = function moosFunctionHandler() {
+    alert("Hello World!");
+}
+
+button.addEventListener('click', moosFunction);
