36d9a1a0af4adc0974a8f6b29802289202a8b860
Addison Moyer <addisonbmoyer@gmail.com>
2016-09-30 11:43:49 -0600

    before  it is not working now

--- js/event.js
+++ js/event.js
@@ -1,15 +1,27 @@
-var button = document.getElementById('button1');
-
-var moosFunction = function moosFunctionHandler() {
-    alert("Hello World!");
+// userName button
+var userName;
+var button = document.getElementById('name');
+var enterName = function moosFunctionHandler() {
+   userName = prompt("Enter your Name.");
 }
+button.addEventListener('click', enterName);
 
-button.addEventListener('click', moosFunction);
-
+// startGame button
 var startButton = document.getElementById('startGame');
-
 var startGameHandler = function startMyGame(){
-    prompt("Enter your Name.");
+    if (userName == undefined) {
+        alert("Please Enter a Name.");
+    } else {
+        alert("Are you ready to Play?");
+    }
+}
+startButton.addEventListener('click', startGameHandler);
+
+var gameButton = function GameButtonConstruct (name){
+    var button = document.getElementById(this.name);
+    console.log(this.name);
 }
 
-startButton.addEventListener('click', startGameHandler);
+var fireWeaponButton = new gameButton('fireweapon');
+
+console.log(fireWeaponButton.button);
