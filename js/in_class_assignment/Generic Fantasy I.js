259ea365d43d8611392c3e8045b93fe896b22db6
Addison Moyer <addisonbmoyer@gmail.com>
2016-10-10 11:46:56 -0600

    trying to get the basics down..

--- js/in_class_assignment/Generic Fantasy I.js
+++ js/in_class_assignment/Generic Fantasy I.js
@@ -1,4 +1,25 @@
 
+// making a user button
+var playerUser;
+var button = document.getElementById('name');
+var enterName = function userButton() {
+   playerUser = prompt("What is your Player Name");
+}
+button.addEventListener('click', enterName);
 
-var playerUser = prompt ("What is your Player Name");
 
+var health = 10,
+var strength = "high",
+var speed = 50,
+
+ var stealth = if (health >= 6) {
+    stealth = "high";
+}else {stealth = "low";}
+
+var enimies3 
+
+enimies.speed
+
+alert(playerUser.speed);
+
+prompt ("what weapon do you use?");
